5e9851a56ae4c280fe6f6b730a6879ef355636db
Ubuntu <ubuntu@ip-172-31-2-148.ec2.internal>
2015-11-11 01:19:33 +0000

    changes to trim and diginorm

--- diginorm_mmetsp.py
+++ diginorm_mmetsp.py
@@ -14,10 +14,18 @@ import clusterfunc
 
 def run_diginorm(diginormdir,interleavedir):
 	print os.listdir(interleavedir)
-	diginorm_string1="python /usr/local/share/khmer/scripts/normalize-by-median.py -p -k 20 "+interleavedir+"*.fq"
-	s=subprocess.Popen(diginorm_string1,shell=True)
+	os.chdir("/home/ubuntu/khmer/scripts/")
+	print os.getcwd()
+	with open("diginorm.sh","w") as diginormfile:
+		diginormfile.write("sudo python normalize-by-median.py -p -k 20 -C 20 -M 4e9 \\"+"\n")
+		diginormfile.write("--savegraph "+diginormdir+"norm.C20k20.ct -u \\"+"\n")
+		diginormfile.write(diginormdir+"orphans.fq.gz \\"+"\n")
+		diginormfile.write(interleavedir+"*.fq"+"\n")
+	s=subprocess.Popen("sudo bash diginorm.sh",shell=True)
 	s.wait()
-	#print diginorm_string1
+	#s=subprocess.Popen("cat diginorm.sh",shell=True)
+	#s.wait()
+	os.chdir("/home/ubuntu/MMETSP")
 
 basedir="/mnt/mmetsp/"
 interleavedir="/mnt/mmetsp/subset/trim_combined/interleave/"
