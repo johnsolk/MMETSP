520c596adc7e29a22908e5d60790a133d68d3253
Ubuntu <ubuntu@ip-172-31-2-148.ec2.internal>
2015-11-11 22:54:56 +0000

    changes to diginorm script

--- diginorm_mmetsp.py
+++ diginorm_mmetsp.py
@@ -11,6 +11,32 @@ import glob
 # custom Lisa module
 import clusterfunc
 
+def run_filter_abund(diginormdir):
+	print os.listdir(diginormdir)
+	os.chdir("/home/ubuntu/khmer/scripts/")
+	with open("trim.sh","w") as trimfile:
+		trimfile.write("sudo python filter-abund.py -V -Z 18 "+diginormdir+"norm.C20k20.ct "+diginormdir+"*.keep && \\"+"\n")
+		trimfile.write("rm "+diginormdir+"*.keep "+diginormdir+"norm.C20k20.ct"+"\n")
+	s=subprocess.Popen("sudo bash trim.sh",shell=True)
+	s.wait()
+	#s=subprocess.Popen("cat trim.sh",shell=True)
+	#s.wait()
+	os.chdir("/home/ubuntu/MMETSP/")
+
+
+def rename_files(diginormdir):
+	print os.listdir(diginormdir)
+	os.chdir("/home/ubuntu/khmer/scripts/")
+	with open("rename.sh","w") as renamefile:
+		renamefile.write("for file in "+diginormdir+"*.pe.*.abundfilt"+"\n")
+		renamefile.write("do"+"\n")
+		renamefile.write("\textract-paired-reads.py ${file} && \\"+"\n")
+		renamefile.write("\t\trm ${file}"+"\n")
+		renamefile.write("done"+"\n")
+	s=subprocess.Popen("cat rename.sh",shell=True)
+	s.wait()
+	os.chdir("/home/ubuntu/MMETSP/")
+
 
 def run_diginorm(diginormdir,interleavedir):
 	print os.listdir(interleavedir)
@@ -25,10 +51,23 @@ def run_diginorm(diginormdir,interleavedir):
 	s.wait()
 	#s=subprocess.Popen("cat diginorm.sh",shell=True)
 	#s.wait()
-	os.chdir("/home/ubuntu/MMETSP")
+	os.chdir("/home/ubuntu/MMETSP/")
+
+def combine_orphaned():
+	gzip -9c orphans.fq.gz.keep.abundfilt > orphans.keep.abundfilt.fq.gz && \
+    rm orphans.fq.gz.keep.abundfilt
+for file in *.pe.*.abundfilt.se
+do
+   gzip -9c ${file} >> orphans.keep.abundfilt.fq.gz && \
+        rm ${file}
+done
+
+
 
 basedir="/mnt/mmetsp/"
 interleavedir="/mnt/mmetsp/subset/trim_combined/interleave/"
 diginormdir="/mnt/mmetsp/diginorm/"
 clusterfunc.check_dir(diginormdir)
-run_diginorm(diginormdir,interleavedir)
+#run_diginorm(diginormdir,interleavedir)
+#run_filter_abund(diginormdir)
+rename_files(diginormdir)
