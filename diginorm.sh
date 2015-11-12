1da0067a8133ae0b29ac900890782a5ba9ad0a2f
Ubuntu <ubuntu@ip-172-31-2-148.ec2.internal>
2015-11-12 05:50:11 +0000

    .sh files

---
+++ diginorm.sh
@@ -0,0 +1,4 @@
+sudo python normalize-by-median.py -p -k 20 -C 20 -M 4e9 \
+--savegraph /mnt/mmetsp/diginorm/norm.C20k20.ct -u \
+/mnt/mmetsp/subset/trim_combined/orphans.fq.gz \
+/mnt/mmetsp/subset/trim_combined/interleave/*.fq
