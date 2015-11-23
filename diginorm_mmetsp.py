0bd94c926547219f92d971e17cec9107e84d88e5
ljcohen <ljcohen@ucdavis.edu>
2015-11-23 02:33:33 +0000

    added checks to diginorm.mmetsp.py

--- diginorm_mmetsp.py
+++ diginorm_mmetsp.py
@@ -12,65 +12,76 @@ import glob
 import clusterfunc
 
 def run_filter_abund(diginormdir):
-	print os.listdir(diginormdir)
-	os.chdir("/home/ubuntu/khmer/scripts/")
-	with open("trim.sh","w") as trimfile:
-		trimfile.write("sudo python filter-abund.py -V -Z 18 "+diginormdir+"norm.C20k20.ct "+diginormdir+"*.keep && \\"+"\n")
-		trimfile.write("rm "+diginormdir+"*.keep "+diginormdir+"norm.C20k20.ct"+"\n")
-	s=subprocess.Popen("sudo bash trim.sh",shell=True)
-	s.wait()
-	#s=subprocess.Popen("cat trim.sh",shell=True)
-	#s.wait()
-	os.chdir("/home/ubuntu/MMETSP/")
+	if glob.glob(diginormdir+"*keep.abundfilt*"):
+		print "filter-abund.py already run"
+	else:
+		os.chdir(diginormdir)
+		with open("filter_abund.sh","w") as abundfile:
+			abundfile.write("sudo python /home/ubuntu/khmer/scripts/filter-abund.py -V -Z 18 "+diginormdir+"norm.C20k20.ct "+diginormdir+"*.keep"+"\n")
+			#trimfile.write("rm "+diginormdir+"*.keep "+diginormdir+"norm.C20k20.ct"+"\n")
+		s=subprocess.Popen("sudo bash filter_abund.sh",shell=True)
+		s.wait()
+		#s=subprocess.Popen("cat trim.sh",shell=True)
+		#s.wait()
+		os.chdir("/home/ubuntu/MMETSP/")
 
 
 def rename_files(diginormdir):
-	print os.listdir(diginormdir)
-	os.chdir("/home/ubuntu/khmer/scripts/")
-	with open("rename.sh","w") as renamefile:
-		renamefile.write("for file in "+diginormdir+"*.abundfilt"+"\n")
-		renamefile.write("do"+"\n")
-		renamefile.write("\tpython extract-paired-reads.py ${file} && \\"+"\n")
-		renamefile.write("\t\trm ${file}"+"\n")
-		renamefile.write("done"+"\n")
-	#s=subprocess.Popen("cat rename.sh",shell=True)
-	#s.wait()
-	s=subprocess.Popen("sudo bash rename.sh",shell=True)
-	s.wait()
-	os.chdir("/home/ubuntu/MMETSP/")
-
+	if glob.glob(diginormdir+"*.abundfilt.pe"):
+		print "paired reads already extracted"
+	else:
+		os.chdir(diginormdir)
+		with open("rename.sh","w") as renamefile:
+			renamefile.write("for file in *.abundfilt"+"\n")
+			renamefile.write("do"+"\n")
+			renamefile.write("\tpython /home/ubuntu/khmer/scripts/extract-paired-reads.py ${file} && \\"+"\n")
+			renamefile.write("\t\trm ${file}"+"\n")
+			renamefile.write("done"+"\n")
+		#s=subprocess.Popen("cat rename.sh",shell=True)
+		#s.wait()
+		s=subprocess.Popen("sudo bash rename.sh",shell=True)
+		s.wait()
+		os.chdir("/home/ubuntu/MMETSP/")
 
 def run_diginorm(diginormdir,interleavedir):
-	print os.listdir(interleavedir)
-	os.chdir("/home/ubuntu/khmer/scripts/")
-	print os.getcwd()
-	with open("diginorm.sh","w") as diginormfile:
-		diginormfile.write("sudo python normalize-by-median.py -p -k 20 -C 20 -M 4e9 \\"+"\n")
-		diginormfile.write("--savegraph "+diginormdir+"norm.C20k20.ct -u \\"+"\n")
-		diginormfile.write("/mnt/mmetsp/subset/trim_combined/orphans.fq.gz \\"+"\n")
-		diginormfile.write(interleavedir+"*.fq"+"\n")
-	s=subprocess.Popen("sudo bash diginorm.sh",shell=True)
-	s.wait()
-	#s=subprocess.Popen("cat diginorm.sh",shell=True)
-	#s.wait()
-	os.chdir("/home/ubuntu/MMETSP/")
+	# this will create and run a script from the working directory
+	# output *.keep files will be in the working directory
+	if glob.glob(diginormdir+"*keep*"):
+		print "normalize-by-median.py already run"
+	else:
+		os.chdir(diginormdir)
+		with open("diginorm.sh","w") as diginormfile:
+			diginormfile.write("sudo python /home/ubuntu/khmer/scripts/normalize-by-median.py -p -k 20 -C 20 -M 4e9 \\"+"\n")
+			diginormfile.write("--savegraph "+diginormdir+"norm.C20k20.ct -u \\"+"\n")
+			diginormfile.write("/mnt/mmetsp/subset/trim_combined/orphans.fq.gz \\"+"\n")
+			diginormfile.write(interleavedir+"*.fq"+"\n")
+		s=subprocess.Popen("sudo bash diginorm.sh",shell=True)
+		s.wait()
+		#s=subprocess.Popen("cat diginorm.sh",shell=True)
+		#s.wait()
+		os.chdir("/home/ubuntu/MMETSP/")
 
 def combine_orphaned(diginormdir):
-	print os.listdir(diginormdir)
-	with open("combine_orphaned.sh","w") as combinedfile:
-		combinedfile.write("gzip -9c "+diginormdir+"orphans.fq.gz.keep.abundfilt > "+diginormdir+"orphans.keep.abundfilt.fq.gz && \\"+"\n")
-		combinedfile.write("\trm "+diginormdir+"orphans.fq.gz.keep.abundfilt"+"\n")
-		combinedfile.write("for file in "+diginormdir+"*.se"+"\n")
-		combinedfile.write("do"+"\n")
-		combinedfile.write("\tgzip -9c ${file} >> orphans.keep.abundfilt.fq.gz && \\"+"\n")
-        	combinedfile.write("\t\trm ${file}"+"\n")
-		combinedfile.write("done"+"\n")
-	#s=subprocess.Popen("cat combine_orphaned.sh",shell=True)
-	#s.wait()
-	s=subprocess.Popen("sudo bash combine_orphaned.sh",shell=True)
-	s.wait()
+	if glob.glob(diginormdir+"orphans*.pe"):
+		print "orphan reads already combined"
+	else:
+		os.chdir(diginormdir)
+		print "combinding orphans now..."
+		with open("combine_orphaned.sh","w") as combinedfile:
+			combinedfile.write("gzip -9c "+diginormdir+"orphans.fq.gz.keep.abundfilt > "+diginormdir+"orphans.keep.abundfilt.fq.gz && \\"+"\n")
+			combinedfile.write("\trm "+diginormdir+"orphans.fq.gz.keep.abundfilt"+"\n")
+			combinedfile.write("for file in "+diginormdir+"*.se"+"\n")
+			combinedfile.write("do"+"\n")
+			combinedfile.write("\tgzip -9c ${file} >> orphans.keep.abundfilt.fq.gz && \\"+"\n")
+        		combinedfile.write("\t\trm ${file}"+"\n")
+			combinedfile.write("done"+"\n")
+		#s=subprocess.Popen("cat combine_orphaned.sh",shell=True)
+		#s.wait()
+		s=subprocess.Popen("sudo bash combine_orphaned.sh",shell=True)
+		s.wait()
 	
 def rename_pe(diginormdir):
+	print "renaming pe files now..."
 	with open("rename.sh","w") as renamefile:
 		renamefile.write("for file in "+diginormdir+"*trimmed.interleaved.fq.keep.abundfilt.pe"+"\n")
 		renamefile.write("do"+"\n")
@@ -87,18 +98,11 @@ basedir="/mnt/mmetsp/"
 interleavedir="/mnt/mmetsp/subset/trim_combined/interleave/"
 diginormdir="/mnt/mmetsp/diginorm/"
 clusterfunc.check_dir(diginormdir)
-#run_diginorm(diginormdir,interleavedir)
-# want to see more about the naming of the output files
-# outputs files to /home/ubuntu/khmer/scripts/
-# mv /home/ubuntu/khmer/scripts/*keep* /mnt/mmetsp/diginorm/ 
-#run_filter_abund(diginormdir)
-# outputs files to /home/ubuntu/khmer/scripts
-# mv /home/ubuntu/khmer/scripts/*.abundfilt /mnt/mmetsp/diginorm/  
-#rename_files(diginormdir)
-# this will output files to /home/ubuntu/khmer/scripts
-# 11/12/2015 there is a problem with Exception: no paired reads!? check file formats...
-# check in ~/MMETSP
-# mv /home/ubuntu/khmer/scripts/orphans.* /mnt/mmetsp/diginorm/
-# mv /home/ubuntu/khmer/scripts/*keep* /mnt/mmetsp/diginorm/
-#combine_orphaned(diginormdir)
+run_diginorm(diginormdir,interleavedir)
+run_filter_abund(diginormdir)
+rename_files(diginormdir)
+combine_orphaned(diginormdir)
 rename_pe(diginormdir)
+# todo: write checks to see if script files exist
+# write more verbose message, e.g. "Done"
+# remove excess files
--- rename.sh
+++ rename.sh
@@ -1,4 +1,4 @@
-for file in /mnt/mmetsp/diginorm/*.abundfilt.pe
+for file in /mnt/mmetsp/diginorm/*trimmed.interleaved.fq.keep.abundfilt.pe
 do
 	newfile=${file%%.fq.keep.abundfilt.pe}.keep.abundfilt.fq
 	mv ${file} ${newfile}
