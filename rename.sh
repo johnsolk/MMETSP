e39193e8280e72fdc5486c59fa72318d50e607aa
root <ljcohen@ucdavis.edu>
2015-11-24 21:45:08 +0000

    removed excess files

--- rename.sh
+++
@@ -1,6 +0,0 @@
-for file in /mnt/mmetsp/diginorm/*trimmed.interleaved.fq.keep.abundfilt.pe
-do
-	newfile=${file%%.fq.keep.abundfilt.pe}.keep.abundfilt.fq
-	mv ${file} ${newfile}
-	gzip ${newfile}
-done
