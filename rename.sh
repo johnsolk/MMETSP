ee8868c98bc19190290a91bc745a9f9429f63878
root <ljcohen@ucdavis.edu>
2015-11-26 17:35:40 +0000

    cleaning up files

--- rename.sh
+++
@@ -1,6 +0,0 @@
-for file in /mnt/mmetsp/Micromonas_pusilla/SRR1300457/diginorm/*trimmed.interleaved.fq.keep.abundfilt.pe
-do
-	newfile=${file%%.fq.keep.abundfilt.pe}.keep.abundfilt.fq
-	mv ${file} ${newfile}
-	gzip ${newfile}
-done
