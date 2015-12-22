d9fd1378b6b4c60c92196c2d86d5dd6f1514dcea
root <ljcohen@ucdavis.edu>
2015-12-22 14:59:00 +0000

    edited Trinity path in assembly.py, edited clean.py to delete duplicate files with split reads

--- clean.py
+++ clean.py
@@ -55,32 +55,67 @@ def execute(url_data):
             		newdir=org_seq_dir+sra+"/"
 			filename=newdir+sra
 			print filename
-			if os.path.isfile(filename+"_1.fastq"):
-				delete_file(filename+"_1.fastq")
-			if os.path.isfile(filename+"_2.fastq"):
-				delete_file(filename+"_2.fastq")	
-	    		if os.path.isfile(filename):
-	    			print "File exists:",filename 	
-				delete_file(filename)
-			diginormdir=newdir+"diginorm/"
-			os.chdir(diginormdir)
-			diginorm_delete_files=["norm.C20k20.ct","orphans.fq.gz.keep.abundfilt"]
-			for filename in glob.glob("*.keep"):
-				diginorm_delete_files.append(filename)
-			for filename in glob.glob("*.abundfilt"):
-				diginorm_delete_files.append(filename)
-			for filename in glob.glob("*.abundfilt.pe"):
-				diginorm_delete_files.append(filename)
-			for filename in glob.glob("*.abundfilt.se"):
-				diginorm_delete_files.append(filename)
-			print diginorm_delete_files
-			for filetodelete in diginorm_delete_files:
-				if os.path.isfile(filetodelete):
-					print "File in diginorm to delete exists:",filetodelete
-					delete_file(filetodelete)
-				else:
-					print "File in diginorm not found:",filetodelete
-			os.chdir("/home/ubuntu/MMETSP/")
+			
+			##
+			# run this after trim_qc.py
+			##
+
+			#if os.path.isfile(filename+"_1.fastq"):
+			#	delete_file(filename+"_1.fastq")
+			#if os.path.isfile(filename+"_2.fastq"):
+			#	delete_file(filename+"_2.fastq")	
+			
+			##
+			# run this after getdata.py
+			##
+	    		#if os.path.isfile(filename):
+			#	print "File exists:",filename 	
+			#	delete_file(filename)
+		
+
+			##
+			# run this after all diginorm steps have finished
+			##
+	
+			#diginormdir=newdir+"diginorm/"
+			#os.chdir(diginormdir)
+			#diginorm_delete_files=["norm.C20k20.ct","orphans.fq.gz.keep.abundfilt"]
+			#for filename in glob.glob("*.keep"):
+		#		diginorm_delete_files.append(filename)
+		#	for filename in glob.glob("*.abundfilt"):
+		#		diginorm_delete_files.append(filename)
+		#	for filename in glob.glob("*.abundfilt.pe"):
+		#		diginorm_delete_files.append(filename)
+		#	for filename in glob.glob("*.abundfilt.se"):
+		#		diginorm_delete_files.append(filename)
+		#	print diginorm_delete_files
+		#	for filetodelete in diginorm_delete_files:
+		#		if os.path.isfile(filetodelete):
+		#			print "File in diginorm to delete exists:",filetodelete
+		#			delete_file(filetodelete)
+		#		else:
+		#			print "File in diginorm not found:",filetodelete
+		#	os.chdir("/home/ubuntu/MMETSP/")
+
+			##
+			# run this after assembly.py
+			# to delete extra files
+			##
+				
+			trinitydir=newdir+"trinity/"
+			os.chdir(trinitydir)
+			listoffiles=os.listdir(trinitydir)
+			filestodelete=[]
+			for filename in listoffiles:
+				if filename.endswith(".fq.gz.1"):
+					filestodelete.append(filename)
+				if filename.endswith(".fq.gz.2"):
+					filestodelete.append(filename)
+			print "These files will be deleted:",filestodelete
+			for i in filestodelete:
+				delete_file(i)
+
+
 basedir="/mnt/mmetsp/"
 datafile="MMETSP_SRA_Run_Info_subset_b.csv"
 url_data=get_data(datafile)
