aaeb4d1992f5a226eb3730be01453b471ef76845
root <root@ip-172-31-7-101.ec2.internal>
2016-02-03 23:21:32 +0000

    added MMETSP_transrate.data.csv

--- report.py
+++ report.py
@@ -10,6 +10,15 @@ import shutil
 import glob
 # custom Lisa module
 import clusterfunc
+# Python plotting libraries
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy import stats, integrate
+import seaborn as sns
+sns.set(color_codes=True)
+
 
 
 def get_data(thefile):
@@ -27,7 +36,7 @@ def get_data(thefile):
             read_type=line_data[position_reads]
             ftp=line_data[position_ftp]
             name_read_tuple=(name,read_type)
-            print name_read_tuple
+            #print name_read_tuple
             #check to see if Scientific Name and run exist
             if name_read_tuple in url_data.keys():
                 #check to see if ftp exists
@@ -45,8 +54,9 @@ def fix_fasta(trinity_fasta,trinity_dir,sample):
 	fix="""
 sed 's_|_-_g' {} > {}
 """.format(trinity_fasta,trinity_out)
-	s=subprocess.Popen(fix,shell=True)
-        s.wait()
+	#s=subprocess.Popen(fix,shell=True)
+	#print fix
+        #s.wait()
 	os.chdir("/home/ubuntu/MMETSP/")
 	return trinity_out
 	
@@ -54,13 +64,25 @@ def transrate(trinitydir,transrate_out,trinity_fasta):
 	transrate_command="""
 transrate -o {} --assembly {}
 """.format(transrate_out,trinity_fasta)
-	s=subprocess.Popen(transrate_command,shell=True)
-	s.wait()
+	#print transrate_command
+	#s=subprocess.Popen(transrate_command,shell=True)
+	#s.wait()
 
-def execute(url_data,basedir):
+def parse_transrate_stats(transrate_assemblies):
+        data=pd.DataFrame.from_csv(transrate_assemblies,header=0,sep=',')
+        return data
+
+def build_DataFrame(data_frame,transrate_data):
+        #columns=["n_bases","gc","gc_skew","mean_orf_percent"]
+	frames=[data_frame,transrate_data]
+	data_frame=pd.concat(frames)
+	return data_frame
+
+def execute(data_frame,url_data,basedir,extra_dir):
 	trinity_fail=[]
+	# construct an empty pandas dataframe to add on each assembly.csv to
 	for item in url_data.keys():
-		print item
+		#print item
 		organism=item[0]
 		sample="_".join(item)
 		org_seq_dir=basedir+organism+"/"
@@ -74,25 +96,64 @@ def execute(url_data,basedir):
 			clusterfunc.check_dir(transrate_dir)
 			#trinity_fasta=dammit_dir+"Trinity.fasta.dammit.fasta"
 			trinity_fasta=trinitydir+"Trinity.fasta"
-			transrate_out=transrate_dir+sample
+			transrate_out=transrate_dir+sample+"/"
 			if os.path.isfile(trinity_fasta):
 				#transrate(dammit_dir)
-		        	fixed_trinity=fix_fasta(trinity_fasta,trinitydir,sample)
-				transrate(trinitydir,transrate_out,fixed_trinity)
+		        	#print transrate_out
+				#fixed_trinity=fix_fasta(trinity_fasta,trinitydir,sample)
+				#transrate(trinitydir,transrate_out,fixed_trinity)
+				transrate_assemblies=transrate_out+"assemblies.csv"
+				data=parse_transrate_stats(transrate_assemblies)
+				data_frame=build_DataFrame(data_frame,data)
 			else:
 				print "Trinity failed:",newdir
 				trinity_fail.append(newdir)	
 	print "This is the number of times Trinity failed:"
 	print len(trinity_fail)
 	print trinity_fail
+	return data_frame
+
+def get_extra_assemblies(extra_dir,data_frame):
+	listofassemblies=os.listdir(extra_dir)
+	for assembly_fasta in listofassemblies:
+                sample_info=assembly_fasta.split("_")
+                sra=sample_info[2].split(".")[0]
+		sample_info=sample_info[:-1]
+		sample_info.append(sra)
+		sample="_".join(sample_info)
+		#print sample
+                transrate_out=extra_dir+sample+"/"
+                fixed_trinity=fix_fasta(assembly_fasta,extra_dir,sample)
+                transrate(extra_dir,transrate_out,fixed_trinity)
+                transrate_assemblies=transrate_out+"assemblies.csv"
+                data=parse_transrate_stats(transrate_assemblies)
+                data_frame=build_DataFrame(data_frame,data)
+	return data_frame
+
+
+def get_histogram(data_frame):
+	gc=pd.DataFrame.hist(data_frame,column="gc")
+	gc_skew=pd.DataFrame.hist(data_frame,column="gc_skew")
+	mean_orf_percent=pd.DataFrame.hist(data_frame,column="mean_orf_percent")
+	n_bases=pd.DataFrame.hist(data_frame,column="n_bases")
+	gc.plot.hist.savefig("/home/ubuntu/MMETSP/gc_hist.png")
+	
+	
+
 
 # basedir:datafile
 file_locations={"/mnt/mmetsp/":"MMETSP_SRA_Run_Info_subset_d.csv",
 		"/mnt/mmetsp1/mmetsp/":"MMETSP_SRA_Run_Info_subset_a.csv",
 		"/mnt/mmetsp2/mmetsp/":"MMETSP_SRA_Run_Info_subset_b.csv"}
 #datafile="MMETSP_SRA_Run_Info_subset2.csv"
+extra_dir="/mnt/mmetsp3/"
+data_frame=pd.DataFrame()
 for basedir in file_locations.keys():
 	datafile=file_locations[basedir]
 	url_data=get_data(datafile)
-	print url_data
-	execute(url_data,basedir)
+	#print url_data
+	data_frame=execute(data_frame,url_data,basedir,extra_dir)
+data_frame=get_extra_assemblies(extra_dir,data_frame)
+print data_frame
+data_frame.to_csv("MMETSP_transrate_data.csv")
+#get_histogram(data_frame)
