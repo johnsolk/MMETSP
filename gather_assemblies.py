95768d113e1ca6f7b164150983e459e7b43dcba6
root <root@ip-172-31-7-101.ec2.internal>
2016-02-03 07:51:04 +0000

    added report.py

---
+++ gather_assemblies.py
@@ -0,0 +1,45 @@
+import os
+import os.path
+from os.path import basename
+from urllib import urlopen
+from urlparse import urlparse
+import subprocess
+from subprocess import Popen, PIPE
+import urllib
+import shutil
+import glob
+# custom Lisa module
+import clusterfunc
+
+def get_data(thefile):
+    count=0
+    url_data={}
+    with open(thefile,"rU") as inputfile:
+        headerline=next(inputfile).split(',')
+        #print headerline        
+        position_name=headerline.index("ScientificName")
+        position_reads=headerline.index("Run")
+        position_ftp=headerline.index("download_path")
+        for line in inputfile:
+            line_data=line.split(',')
+            name="_".join(line_data[position_name].split())
+            read_type=line_data[position_reads]
+            ftp=line_data[position_ftp]
+            name_read_tuple=(name,read_type)
+            print name_read_tuple
+            #check to see if Scientific Name and run exist
+            if name_read_tuple in url_data.keys():
+                #check to see if ftp exists
+                if ftp in url_data[name_read_tuple]:
+                    print "url already exists:", ftp
+                else:
+                    url_data[name_read_tuple].append(ftp)
+            else:
+                url_data[name_read_tuple] = [ftp]
+        return url_data
+
+
+
+
+
+fasta_file="Trinity.fasta.dammit.fasta"
