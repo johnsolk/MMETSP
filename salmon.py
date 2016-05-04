d872b128d41b55cd446fa6d0484481711510ceab
Lisa Cohen <ljcohen@ucdavis.edu>
2016-05-04 14:33:52 +0000

    added Micromonas_pusilla_SRR1300458 transcriptome assembly

--- salmon.py
+++ salmon.py
@@ -14,6 +14,10 @@ import clusterfunc
 def get_data(thefile):
     count=0
     url_data={}
+    if os.path.isfile(thefile):
+	print "file exists",thefile
+    else:
+	print os.getcwd()
     with open(thefile,"rU") as inputfile:
         headerline=next(inputfile).split(',')
         #print headerline        
@@ -41,11 +45,13 @@ def get_data(thefile):
 def salmon_index(salmondir,sra,trinity_fasta):
 	index=sra+"_index"
 	os.chdir(salmondir)
+	if os.path.isfile(trinity_fasta):
+		print "file exists:",trinity_fasta
 	salmon_index="salmon index --index "+index+" --transcripts "+trinity_fasta+" --type quasi"
-	#print salmon_index	
-	#s=subprocess.Popen(salmon_index,shell=True)
-	#s.wait()
-	#print "Indexed."
+	print salmon_index	
+	s=subprocess.Popen(salmon_index,shell=True)
+	s.wait()
+	print "Indexed."
 	os.chdir("/home/ubuntu/MMETSP/")
 	return index
 
@@ -58,10 +64,29 @@ def quant_salmon(salmondir,index,sra,newdir):
 	if os.path.isfile(file2):
 		print "file exists:",file2
 	salmon_string="salmon quant -i "+index+" --libType IU -1 "+file1+" -2 "+file2+" -o "+salmondir+sra+".quant"
-	print salmon_string
+	os.chdir("/home/ubuntu/MMETSP/")
         s=subprocess.Popen(salmon_string,shell=True)
 	s.wait()
+	os.chdir("/home/ubuntu/MMETSP/")
 	
+
+
+def gather_counts(salmondir):
+	os.chdir(salmondir)
+	gather_counts="python /home/ubuntu/MMETSP/gather-counts.py"
+	print os.getcwd()
+	print gather_counts
+	#s=subprocess.Popen(gather_counts,shell=True)
+        #s.wait()
+	os.chdir("/home/ubuntu/MMETSP/")	
+
+def sim_link(salmondir,sra):
+	counts_files_dir="/home/ubuntu/MMETSP/counts/"
+	clusterfunc.check_dir(counts_files_dir)
+	link_command="ln -fs "+salmondir+"*.counts "+counts_files_dir+sra+".counts" 
+	print link_command
+
+
 def execute(url_data):
 	for item in url_data.keys():
 		organism=item[0]
@@ -74,11 +99,18 @@ def execute(url_data):
 			salmondir=newdir+"salmon/"
 			clusterfunc.check_dir(salmondir)
 			trinity_fasta=trinitydir+"Trinity.fasta"
-			index=salmon_index(salmondir,sra,trinity_fasta)
-			quant_salmon(salmondir,index,sra,newdir)
-
-basedir="/mnt/mmetsp/"
-datafile="MMETSP_SRA_Run_Info_subset2.csv"
-url_data=get_data(datafile)
-print url_data
-execute(url_data)
+			#index=salmon_index(salmondir,sra,trinity_fasta)
+			#quant_salmon(salmondir,index,sra,newdir)
+			#gather_counts(salmondir)		
+			sim_link(salmondir,sra)			
+	
+# The following dictionary is formatted as
+# basedir:datafile
+file_locations={"/mnt2/mmetsp/":"MMETSP_SRA_Run_Info_subset_d.csv",
+                "/mnt3/mmetsp/":"MMETSP_SRA_Run_Info_subset_a.csv",
+                "/mnt4/mmetsp/":"MMETSP_SRA_Run_Info_subset_b.csv"}
+for basedir in file_locations.keys():
+        datafile=file_locations[basedir]
+        mmetsp_data=get_data(datafile)
+        print mmetsp_data
+        execute(mmetsp_data)
