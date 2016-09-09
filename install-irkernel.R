570b43be388d10762d6382329169cb7d8b64dedc
C. Titus Brown <titus@idyll.org>
2016-09-09 10:41:06 -0700

    removed some more stuff

--- install-irkernel.R
+++
@@ -1,5 +0,0 @@
-install.packages(c('rzmq','repr','IRkernel','IRdisplay'),
-                 repos = c('http://irkernel.github.io/',
-                           'http://cran.us.r-project.org'))
-
-IRkernel::installspec()
