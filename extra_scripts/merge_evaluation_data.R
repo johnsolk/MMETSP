20e688cd65b91ea1a88a196beb4a24a88bc49cac
Lisa Cohen <ljcohen@ucdavis.edu>
2016-09-27 21:42:31 -0700

    all the correlations

--- extra_scripts/merge_evaluation_data.R
+++ extra_scripts/merge_evaluation_data.R
@@ -6,7 +6,7 @@ busco_scores<-read.csv("assembly_evaluation_data/busco_scores.csv")
 reads<-read.table("assembly_evaluation_data/trimmomatic_reads_table.txt",sep="\t",header=TRUE)
 unique_kmers<-read.csv("assembly_evaluation_data/unique_kmers.txt",sep="\t",header=TRUE)
 MMETSP_ncbi<-read.csv("SraRunInfo.csv")
-mmetsp_id<-read.table("imicrobe/MMETSP_list",sep="\t",header=TRUE)
+mmetsp_id<-read.csv("imicrobe/MMETSP_Table.csv")
 
 
 head(transrate)
@@ -19,20 +19,18 @@ head(reads)
 dim(reads)
 head(unique_kmers)
 dim(unique_kmers)
+head(mmetsp_id)
+dim(mmetsp_id)
 head(MMETSP_ncbi)
 dim(MMETSP_ncbi)
-head(mmetsp_id)
-dim(mmetsp_id) # this is only 176
+
 
 evaluation_matrix_1<-merge(transrate,transrate_reference,by="Run")
 evaluation_matrix_2<-merge(evaluation_matrix_1,busco_scores,by="Run")
 evaluation_matrix_3<-merge(evaluation_matrix_2,reads,by="Run")
 evaluation_matrix_4<-merge(evaluation_matrix_3,unique_kmers,by="Run")
-evaluation_matrix_5<-merge(evaluation_matrix_4,MMETSP_ncbi,by="Run")
-evaluation_matrix<-evaluation_matrix_5
-dim(evaluation_matrix)
-# wait to do this until later when there are more rows
-# evaluation_matrix<-merge(evaluation_matrix_5,mmetsp_id,by="SampleName")
+evaluation_matrix_5<-merge(evaluation_matrix_4,mmetsp_id,by="SampleName")
+evaluation_matrix<-merge(evaluation_matrix_5,MMETSP_ncbi,by="Run")
 
 write.csv(evaluation_matrix,"assembly_evaluation_data/MMETSP_evaluation_matrix.csv")
 colnames(evaluation_matrix)
