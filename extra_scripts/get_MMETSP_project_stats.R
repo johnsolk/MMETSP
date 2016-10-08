e7fcca566e849501f0e308e63c6d8e428852685d
Lisa Cohen <ljcohen@ucdavis.edu>
2016-10-08 16:07:24 -0700

    combined transrate reference metrics added

--- extra_scripts/get_MMETSP_project_stats.R
+++ extra_scripts/get_MMETSP_project_stats.R
@@ -14,9 +14,17 @@ OLlist$Venn_List$imicrobe
 length(unique(MMETSP_id_NCBI))
 length(unique(ncbi$Run))
 length(unique(MMETSP_id_imicrobe))
-ncbi_719<-read.csv("~/Downloads/SraRunInfo_719.csv")
+ncbi_719<-read.csv("SraRunInfo_719.csv")
 length(unique(ncbi_719$Run))
 length(unique(ncbi_719$SampleName))
 extra<-ncbi_719$Run[!(ncbi_719$Run %in% ncbi$Run)]
-ncbi_sra_duplicated_MMETSP<-ncbi[duplicated(ncbi$SampleName),]
+duplicates<-ncbi_719[duplicated(ncbi_719$SampleName),]
+duplicates<-duplicates[,c(30)]
+length(duplicates)
+duplicates<-unique(duplicates)
+length(duplicates)
+duplicated<-ncbi_719[(ncbi_719$SampleName %in% duplicates),]
+duplicated<-duplicated[,c(1,30)]
+duplicated[order(duplicated$SampleName),]
+length(duplicated$SampleName)
 
