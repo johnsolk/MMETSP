b2a17b2ab155481c0dbc73e284fd795c9342457b
Lisa Cohen <ljcohen@ucdavis.edu>
2017-02-18 14:15:44 -0500

    anotation_stats.csv

--- extra_scripts/check_unique_names.py
+++ extra_scripts/check_unique_names.py
@@ -1,25 +1,63 @@
 import os
+import pandas as pd
 
-name_files_dir = "/mnt/home/ljcohen/mmetsp_name_id/"
-names = {}
-name_files_short = os.listdir(name_files_dir)
-name_files = []
-for name_filename in name_files_short:
-    name_file_long = name_files_dir + name_filename
-    name_files.append(name_file_long)
-for name_file in name_files:
-    mmetsp = name_file.split(".")[0].split("/")[5]
-    with open(name_file) as filename:
-        for line in filename:
-            if line.startswith("Name"):
-                if line.split("=")[1].startswith("ORF") == False:
-                    if mmetsp in names:
-                        names[mmetsp].append(line)
-                    else:
-                        names[mmetsp] = [line]
-for mmetsp_entry in names:
-    print(mmetsp_entry)
-    myset = set(names[mmetsp_entry])
-    newlist = list(myset)
-    print("Unique:",len(newlist))
-    print("Total:",len(names[mmetsp_entry]))
+def get_sample_dictionary(sample_dictionary,filename):
+    with open(filename) as outfile:
+        for line in outfile:
+            line_split=line.split(".")
+            #print(line_split)
+            if line_split[1].startswith("/MMETSP"):
+                mmetsp = line_split[1][1:]
+                #print(mmetsp)
+                num  = next(outfile).split()[0]
+                if mmetsp in sample_dictionary:
+                    sample_dictionary[mmetsp].append(num)
+                else:
+                    sample_dictionary[mmetsp] = [num]
+    return sample_dictionary
+
+def build_DataFrame(data_frame, new_data):
+    # columns=["n_bases","gc","gc_skew","mean_orf_percent"]
+    frames = [data_frame, new_data]
+    data_frame = pd.concat(frames,axis=1)
+    #data_frame = data_frame.merge(new_data)
+    return data_frame
+
+def get_table(sample_dictionary):
+    data_frame_new = pd.DataFrame.from_dict(sample_dictionary)
+    return data_frame_new
+ 
+# separately, get total contigs from n_seqs from assembles.csv transrate files
+# separately, get total names and unique names
+
+# put all of these stats into one table
+annotation_stats_files = "/mnt/home/ljcohen/MMETSP/assembly_evaluation_data/"
+total_annotated_contigs = annotation_stats_files + "total_contigs"
+total_OrthoDB = annotation_stats_files + "total_OrthoDB"
+total_Pfam = annotation_stats_files + "total_Pfam"
+total_Rfam = annotation_stats_files + "total_Rfam"
+# these files were sorted by e-value then the top annotation was chosen
+# these are the annotation stats for these reduced annotations
+# there should be one annotation per contig
+unique_annotations = annotation_stats_files + "unique_annotations"
+unique_OrthoDB = annotation_stats_files + "unique_OrthoDB"
+unique_Pfam = annotation_stats_files + "unique_Pfam"
+unique_Rfam = annotation_stats_files + "unique_Rfam"
+# these are the annotations for contigs with false crbb with ncgr ref
+# these are reduced, so one annotation per contig chosen with same method as above
+false_crbb_unique_annotations = annotation_stats_files + "false_crbb_unique_annotations"
+false_crbb_OrthoDB = annotation_stats_files + "false_crbb_OrthoDB"
+false_crbb_Pfam = annotation_stats_files + "false_crbb_Pfam"
+false_crbb_Rfam = annotation_stats_files + "false_crbb_Rfam"
+data_frame = pd.DataFrame()
+annotation_files = [total_annotated_contigs,unique_annotations,false_crbb_unique_annotations,total_OrthoDB,unique_OrthoDB,false_crbb_OrthoDB,total_Pfam,unique_Pfam,false_crbb_Pfam,total_Rfam,unique_Rfam,false_crbb_Rfam]
+for annotation_file in annotation_files:
+    print(annotation_file)
+    sample_dictionary = {}
+    sample_dictionary=get_sample_dictionary(sample_dictionary,annotation_file)
+    #print(sample_dictionary)
+    data_frame_new = get_table(sample_dictionary)
+    data_frame = build_DataFrame(data_frame,data_frame_new) 
+    #print(data_frame)
+out_filename = "/mnt/home/ljcohen/MMETSP/assembly_evaluation_data/annotation_stats.csv"
+data_frame.to_csv(out_filename)
---
+++ extra_scripts/get_unique_names.py
@@ -0,0 +1,50 @@
+import os
+
+def get_unique_names_overall(names_files_dir):
+    names = {}
+    name_files_short = os.listdir(names_files_dir)
+    name_files = []
+    for name_filename in name_files_short:
+        name_file_long = names_files_dir + name_filename
+        name_files.append(name_file_long)
+    for name_file in name_files:
+        mmetsp = name_file.split(".")[0].split("/")[5]
+        with open(name_file) as filename:
+            for line in filename:
+                if line.startswith("Name"):
+                    if line.split("=")[1].startswith("ORF") == False:
+                        if mmetsp in names:
+                            names[mmetsp].append(line)
+                        else:
+                            names[mmetsp] = [line]
+    unique_dict = {}
+    for mmetsp_entry in names:
+        print(mmetsp_entry)
+        myset = set(names[mmetsp_entry])
+        newlist = list(myset)
+        unique = len(newlist)
+        # this number should match total_contigs
+        total = len(names[mmetsp_entry])
+        unique_dict[mmetsp_entry] = [total]
+        unique_dict[mmetsp_entry].append(unique)
+    return unique_dict
+
+def make_out_file(out_file,sample_dictionary):
+    header=["SampleName","total_annotation_names","unique_annotation_names"]
+    with open(out_file,"w") as datafile:
+        datafile.write("\t".join(header))
+        datafile.write("\n")
+        for sample in sample_dictionary:
+            datafile.write(sample+"\t")
+            annotations = sample_dictionary[sample]
+            print(annotations)
+            results = list(map(str, annotations))
+            datafile.write("\t".join(results))
+            datafile.write("\n")
+    datafile.close()
+
+names_files_dir = "/mnt/home/ljcohen/mmetsp_name_id/"
+out_file = "/mnt/home/ljcohen/MMETSP/assembly_evaluation_data/collapse_names"
+unique_dictionary = get_unique_names_overall(names_files_dir)
+make_out_file(out_file,unique_dictionary)
+print("Written:",make_out_file)
